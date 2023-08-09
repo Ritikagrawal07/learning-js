78540189805e37d991294712394aacbfba96dec5
Ritikagrawal07 <103807116+Ritikagrawal07@users.noreply.github.com>
2023-08-09 23:04:49 +0000

    conv

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 13445
+let emailId = "ritik@gmail.com"
+var accountPassword = "Wrestlemaniaaaa1111"
+accountCity = "Ranchi"
+
+// accountId = 2 // not allowed
+emailId = "ha@gmail.com"
+accountPassword = "gwedvsbm"
+accountCity = "banglore"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountPassword,accountCity,emailId,accountState])
+/*
+prefer not to use var because issue in blockscope and functional scope
+*/
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = "44abc"
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// let score1 = null
+// console.log(typeof (score1));
+
+// let valueInNumber1 = Number(score);
+// console.log(typeof (valueInNumber1));
+// console.log(valueInNumber1);
+
+// let score2 = undefined
+// console.log(typeof (score2));
+
+// let valueInNumber2 = Number(score2);
+// console.log(typeof (valueInNumber2));
+// console.log(valueInNumber2);
+
+
+// let score3 = true
+// console.log(typeof (score3));
+
+// let valueInNumber3 = Number(score3);
+// console.log(typeof (valueInNumber3));
+// console.log(valueInNumber3);
+
+// "33"=>33
+// "33abc"=> NaN
+// null=>NaN
+
+let isLoggedIn = 1
+
+let boolISLoggedIn = Boolean(isLoggedIn)
+console.log(boolISLoggedIn);
+
+// 1=>true,0=>false
+// ""=>false
+// "ritik"=>true
