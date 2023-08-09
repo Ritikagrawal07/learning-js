7db6a99e2cf075613cda100af6556807ed2448e0
Ritikagrawal07 <103807116+Ritikagrawal07@users.noreply.github.com>
2023-08-09 22:14:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// "use strict" // treate all js code as newer version
+
+// alert(3+3)  // we are using node js not browser
+// number
+// bigint
+// boolean
+// undefined
+// null
+// symbol => unique
+
+// object
+console.log(typeof "ritik");
+console.log(typeof null);
+console.log(typeof undefined);
